2c0dab3c735878ce7a8fbea3a0277695d82bfe0c
Chuu Htet Naing <chuuhtetnaing@gmail.com>
2022-09-04 02:34:35 +0630

    implemented document parser basic ui with api call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,206 @@
+import DragNDrop from "./DragNDrop";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import fileDownload from "js-file-download";
+import {
+  Alert,
+  Box,
+  Button,
+  CircularProgress,
+  Container,
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  Slider,
+  Snackbar,
+  Stack,
+  styled,
+  TextField,
+} from "@mui/material";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
+
 function App() {
+  const [files, setFiles] = useState([]);
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [reqType, setReqType] = useState("paragraph");
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  useEffect(() => {
+    console.log("files", files);
+  }, [files]);
+
+  const onSubmitFile = () => {
+    setLoading(true);
+    const formData = new FormData();
+    // formData.append('files', files);
+
+    for (let i = 0; i < files.length; i++) {
+      formData.append("files", files[i]);
+    }
+    // formData.append("typee", "figure");
+    // formData.append('fileName', file.name);
+    const config = {
+      responseType: "blob",
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    };
+
+    const url = `http://chuuhtetnaing.ddns.net:8000/uploadfiles/${reqType}/${pageNumber}/`;
+    axios
+      .post(url, formData, config)
+      .then((response) => response.data)
+      .then((data) => {
+        setLoading(false);
+        setOpen(true);
+        fileDownload(data, "result.xlsx");
+      })
+      .catch((err) => console.log("err", err));
+  };
+
+  const PrettoSlider = styled(Slider)({
+    color: "#52af77",
+    height: 8,
+    "& .MuiSlider-track": {
+      border: "none",
+    },
+    "& .MuiSlider-thumb": {
+      height: 24,
+      width: 24,
+      backgroundColor: "#fff",
+      border: "2px solid currentColor",
+      "&:focus, &:hover, &.Mui-active, &.Mui-focusVisible": {
+        boxShadow: "inherit",
+      },
+      "&:before": {
+        display: "none",
+      },
+    },
+    "& .MuiSlider-valueLabel": {
+      lineHeight: 1.2,
+      fontSize: 12,
+      background: "unset",
+      padding: 0,
+      width: 32,
+      height: 32,
+      borderRadius: "50% 50% 50% 0",
+      backgroundColor: "#52af77",
+      transformOrigin: "bottom left",
+      transform: "translate(50%, -100%) rotate(-45deg) scale(0)",
+      "&:before": { display: "none" },
+      "&.MuiSlider-valueLabelOpen": {
+        transform: "translate(50%, -100%) rotate(-45deg) scale(1)",
+      },
+      "& > *": {
+        transform: "rotate(45deg)",
+      },
+    },
+  });
+
+  const onSliderChange = (e) => {
+    console.log(e.target.value);
+    setPageNumber(e.target.value);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   return (
-    <div>
-      <h1>Document Parser Frontend</h1>
-    </div>
+    <Container maxWidth="sm">
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+      >
+        <h1>TheWalnut.AI</h1>
+        <h5>Convert Your Document</h5>
+        <DragNDrop files={files} setFiles={setFiles} />
+        <FormControl>
+          <RadioGroup
+            row
+            aria-labelledby="demo-row-radio-buttons-group-label"
+            name="row-radio-buttons-group"
+            value={reqType}
+            onChange={(e) => setReqType(e.target.value)}
+          >
+            <FormControlLabel
+              value="paragraph"
+              control={<Radio />}
+              label="Paragraph"
+            />
+            <FormControlLabel
+              value="sentence"
+              control={<Radio />}
+              label="Sentence"
+            />
+            <FormControlLabel value="table" control={<Radio />} label="Table" />
+          </RadioGroup>
+        </FormControl>
+        {loading ? (
+          <CircularProgress />
+        ) : (
+          <Button variant="contained" onClick={() => onSubmitFile()}>
+            Extract
+          </Button>
+        )}
+      </Stack>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+      >
+        <Box sx={{ width: 300 }}>
+          <PrettoSlider
+            valueLabelDisplay="auto"
+            aria-label="pretto slider"
+            defaultValue={pageNumber}
+            min={1}
+            max={numPages}
+            marks
+            value={pageNumber}
+            onChange={(e) => onSliderChange(e)}
+          />
+        </Box>
+        <TextField
+          size="small"
+          sx={{ width: 100 }}
+          id="outlined-basic"
+          label="Page No"
+          variant="outlined"
+          value={pageNumber}
+          type="number"
+          onChange={(e) => setPageNumber(parseInt(e.target.value))}
+          inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+        />
+        <div>
+          <div style={{ border: "1px black dotted" }}>
+            <Document file={files[0]} onLoadSuccess={onDocumentLoadSuccess}>
+              <Page height={500} pageNumber={pageNumber} />
+            </Document>
+          </div>
+        </div>
+      </Stack>
+
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+          The excel file is successfully download!
+        </Alert>
+      </Snackbar>
+    </Container>
   );
 }
 
---
+++ src/DragNDrop.js
@@ -0,0 +1,143 @@
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { useDropzone } from "react-dropzone";
+
+const thumbsContainer = {
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  marginTop: 16,
+};
+
+const thumb = {
+  display: "inline-flex",
+  borderRadius: 2,
+  border: "1px solid #eaeaea",
+  marginBottom: 8,
+  marginRight: 8,
+  width: 100,
+  height: 100,
+  padding: 4,
+  boxSizing: "border-box",
+};
+
+const thumbInner = {
+  display: "flex",
+  minWidth: 0,
+  overflow: "hidden",
+};
+
+const img = {
+  display: "block",
+  width: "auto",
+  height: "100%",
+};
+
+const baseStyle = {
+  flex: 1,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "25px",
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: "#eeeeee",
+  borderStyle: "dashed",
+  backgroundColor: "#fafafa",
+  color: "#bdbdbd",
+  outline: "none",
+  transition: "border .24s ease-in-out",
+};
+
+const focusedStyle = {
+  borderColor: "#2196f3",
+};
+
+const acceptStyle = {
+  borderColor: "#00e676",
+};
+
+const rejectStyle = {
+  borderColor: "#ff1744",
+};
+
+export default function DragNDrop(props) {
+  const { files, setFiles } = props;
+
+  const [filePreviews, setFilePreviews] = useState([]);
+  const onDrop = useCallback((acceptedFiles) => {
+    setFilePreviews(
+      acceptedFiles.map((file) =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file),
+        })
+      )
+    );
+
+    setFiles(acceptedFiles);
+
+    acceptedFiles.forEach((file) => {
+      console.log(file);
+      const reader = new FileReader();
+
+      reader.onabort = () => console.log("file reading was aborted");
+      reader.onerror = () => console.log("file reading has failed");
+      reader.onload = () => {
+        // Do whatever you want with the file contents
+        const binaryStr = reader.result;
+        console.log(binaryStr);
+      };
+      reader.readAsArrayBuffer(file);
+    });
+  }, []);
+
+  const { getRootProps, getInputProps, isFocused, isDragAccept, isDragReject } =
+    useDropzone({ onDrop, maxFiles: 1 });
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isFocused ? focusedStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isFocused, isDragAccept, isDragReject]
+  );
+
+  const thumbs = filePreviews.map((file) => (
+    <div style={thumb} key={file.name}>
+      <div style={thumbInner}>
+        <img
+          src={file.preview}
+          style={img}
+          // Revoke data uri after image is loaded
+          onLoad={() => {
+            URL.revokeObjectURL(file.preview);
+          }}
+        />
+      </div>
+    </div>
+  ));
+
+  useEffect(() => {
+    // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
+    return () =>
+      filePreviews.forEach((file) => URL.revokeObjectURL(file.preview));
+  }, []);
+
+  const renderFiles = filePreviews.map((file) => (
+    <li key={file.name}>
+      {file.name} - {file.size} bytes
+    </li>
+  ));
+
+  return (
+    <div className="container">
+      <div {...getRootProps({ style })}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop a PDF file here, or click to select a PDF file</p>
+      </div>
+      {/*<aside style={thumbsContainer}>{thumbs}</aside>*/}
+      {/*<ul>{renderFiles}</ul>*/}
+    </div>
+  );
+}
